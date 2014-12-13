be673f7206b02172bea71671ca04db3ec0032cc0
Divi <sylvain.lorinet@gmail.com>
2014-12-13 21:02:24 +0100

    Initial commit.

---
+++ src/Rocket/ORM/Rocket.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the "RocketORM" package.
+ *
+ * https://github.com/RocketORM/ORM
+ *
+ * For the full license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @author Sylvain Lorinet <sylvain.lorinet@gmail.com>
+ */
+class Rocket
+{
+    const VERSION = '0.1.0';
+
+    const CONNECTION_MODE_WRITE = 0;
+    const CONNECTION_MODE_READ  = 1;
+}
